2d691163ea41891c05596e638739dcb9affc2aa5
Nicholas Benson <nbenson@leapmotion.com>
2017-07-17 18:11:33 -0700

    pushing borken text curving

---
+++ Assets/ParticleEngine/UITests/Common Assets/Scripts/Parameter Glue/SimulatorSliderSetTrailLength.cs
@@ -0,0 +1,26 @@
+﻿using Leap.Unity.GraphicalRenderer;
+using Leap.Unity.Interaction;
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class SimulatorSliderSetTrailLength : SimulatorUIControl {
+
+  public InteractionSlider slider;
+  public LeapTextGraphic textOutput;
+
+  protected override void Reset() {
+    base.Reset();
+
+    slider = GetComponent<InteractionSlider>();
+  }
+
+  void Update() {
+    simulatorSetters.SetTrailSize(slider.HorizontalSliderValue);
+
+    if (textOutput != null) {
+      textOutput.text = slider.HorizontalSliderValue.ToString();
+    }
+  }
+
+}

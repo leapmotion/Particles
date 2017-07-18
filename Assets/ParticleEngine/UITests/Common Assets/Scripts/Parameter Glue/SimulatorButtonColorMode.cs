5092eec4aab2ddf8c072c852b48bdcba4144551d
Nicholas Benson <nbenson@leapmotion.com>
2017-07-17 18:50:30 -0700

    Two working panels

---
+++ Assets/ParticleEngine/UITests/Common Assets/Scripts/Parameter Glue/SimulatorButtonColorMode.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class SimulatorButtonColorMode : SimulatorButtonControl {
+
+  [Header("Which Color Mode")]
+  public TextureSimulator.ColorMode modeOnPress;
+
+  public override void onPress() {
+    simulatorSetters.SetColorMode(modeOnPress);
+  }
+
+}

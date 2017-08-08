c1e0641b2f3330203418620cb9afea9c02d0c033
Amarcolina <Amarcolina@gmail.com>
2017-08-07 17:33:55 -0700

    working

--- Assets/ParticleEngine/Groupshared/ComputeTest.cs
+++ Assets/ParticleEngine/Groupshared/ComputeTest.cs
@@ -1,10 +1,14 @@
 ﻿using System.Collections;
 using UnityEngine;
 using UnityEngine.UI;
+using Leap.Unity.Query;
 
 public class ComputeTest : MonoBehaviour {
 
+  public bool useLayers = true;
   public int[] strides;
+
+  public Material displayMat;
 
   public ComputeShader shader;
   public Text label;
@@ -14,12 +18,13 @@ public class ComputeTest : MonoBehaviour {
   private ComputeBuffer positions;
 
   private int maxParticles;
+  private int layers;
+
+  private int randomizeKernel;
   private int velocityKernel;
   private int positionKernel;
 
   private IEnumerator Start() {
-    yield return new WaitForSeconds(2);
-
     while (true) {
       foreach (var stride in strides) {
         begin(stride);
@@ -34,9 +39,16 @@ public class ComputeTest : MonoBehaviour {
         float endTime = Time.time + perTest;
         int frames = 0;
         while (Time.time < endTime) {
-          shader.Dispatch(velocityKernel, maxParticles / stride, 1, 1);
-          shader.Dispatch(velocityKernel, maxParticles / stride, 1, 1);
-          shader.Dispatch(velocityKernel, maxParticles / stride, 1, 1);
+
+          for (int i = 0; i < 4; i++) {
+            if (useLayers) {
+              shader.Dispatch(velocityKernel, 4096 / stride, layers, 1);
+            } else {
+              shader.Dispatch(velocityKernel, maxParticles / stride, 1, 1);
+            }
+          }
+
+          shader.Dispatch(positionKernel, maxParticles / stride, 1, 1);
           frames++;
           yield return null;
         }
@@ -50,20 +62,50 @@ public class ComputeTest : MonoBehaviour {
     }
   }
 
+  private void OnDisable() {
+    end();
+  }
+
+  private void OnPostRender() {
+    if (positions != null) {
+      displayMat.SetPass(0);
+      Graphics.DrawProcedural(MeshTopology.Points, 4096);
+    }
+  }
+
   private void begin(int stride) {
     end();
 
-    maxParticles = (4096 / stride) * stride;
-    
+    if (useLayers) {
+      layers = 1024 / stride;
+      maxParticles = 4096;
+    } else {
+      maxParticles = (4096 / stride) * stride;
+    }
+
     velocities = new ComputeBuffer(maxParticles, sizeof(float) * 4);
     positions = new ComputeBuffer(maxParticles, sizeof(float) * 4);
 
-    velocityKernel = shader.FindKernel("UpdateVelocity_" + stride);
+    positions.SetData(new Vector4[maxParticles].Fill(() => new Vector3(Random.value, Random.value * 0.1f, Random.value)));
+    velocities.SetData(new Vector4[maxParticles]);
 
-    foreach (var kernel in new int[] { velocityKernel }) {
+    if (useLayers) {
+      velocityKernel = shader.FindKernel("UpdateVelocity_" + stride + "_" + layers);
+    } else {
+      velocityKernel = shader.FindKernel("UpdateVelocity_" + stride);
+    }
+
+    randomizeKernel = shader.FindKernel("RandomizePositions");
+    positionKernel = shader.FindKernel("UpdatePosition");
+
+    foreach (var kernel in new int[] { velocityKernel, positionKernel, randomizeKernel }) {
       shader.SetBuffer(kernel, "_Positions", positions);
       shader.SetBuffer(kernel, "_Velocities", velocities);
     }
+
+    displayMat.SetBuffer("_Positions", positions);
+
+    //shader.Dispatch(randomizeKernel, maxParticles, 1, 1);
   }
 
   private void end() {
@@ -79,8 +121,4 @@ public class ComputeTest : MonoBehaviour {
       positions = null;
     }
   }
-
-
-
-
 }
---
+++ Assets/ParticleEngine/Groupshared/GroupsharedDebug.shader
@@ -0,0 +1,41 @@
+﻿Shader "Unlit/GroupsharedDebug"
+{
+	Properties
+	{
+		_MainTex ("Texture", 2D) = "white" {}
+  }
+  SubShader
+  {
+    Tags { "RenderType" = "Opaque" }
+    LOD 100
+
+    Pass
+    {
+      CGPROGRAM
+      #pragma target 5.0
+      #pragma vertex vert
+      #pragma fragment frag
+      #include "UnityCG.cginc"
+
+      StructuredBuffer<float4> _Positions;
+
+			struct v2f
+			{
+				float4 vertex : SV_POSITION;
+			};
+			
+			v2f vert (uint id : SV_VertexID)
+			{
+				v2f o;
+				o.vertex = UnityObjectToClipPos(_Positions[id].xyz);
+				return o;
+			}
+			
+			fixed4 frag (v2f i) : SV_Target
+			{
+				return float4(0, 1, 1, 1);
+			}
+			ENDCG
+		}
+	}
+}

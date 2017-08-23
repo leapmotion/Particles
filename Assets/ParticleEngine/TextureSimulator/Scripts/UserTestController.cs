159d50b3583804d10cbc43977330fb4933ae52c7
Alex Marcolina <amarcolina@gmail.com>
2017-08-23 13:52:11 -0700

    stuff

--- Assets/ParticleEngine/TextureSimulator/Scripts/UserTestController.cs
+++ Assets/ParticleEngine/TextureSimulator/Scripts/UserTestController.cs
@@ -1,6 +1,7 @@
 ﻿using System.IO;
 using System.Linq;
 using System.Collections;
+using System.Collections.Generic;
 using UnityEngine;
 using UnityEngine.UI;
 using Leap.Unity;
@@ -14,52 +15,113 @@ public class UserTestController : MonoBehaviour {
   public StreamingFolder textFolder;
   public StreamingFolder ecosystemFolder;
 
-  IEnumerator Start() {
-    while (!isButtonPressed) {
-      yield return null;
+  private List<string> _ecosystemPaths;
+  private List<string> _scriptPaths = new List<string>();
+  private int _currEcosystem = -1;
+  private int _currScript = 0;
+
+  void Start() {
+    _ecosystemPaths = Directory.GetFiles(ecosystemFolder.Path).
+                                Where(p => p.EndsWith(".json")).
+                                OrderBy(p => p).
+                                ToList();
+
+    //foreach (var ecosystemPath in ecosystemPaths) {
+    //  string ecosystemName = Path.GetFileNameWithoutExtension(ecosystemPath);
+
+    //  var desc = JsonUtility.FromJson<TextureSimulator.SimulationDescription>(File.ReadAllText(ecosystemPath));
+    //  sim.RestartSimulation(desc, TextureSimulator.ResetBehavior.SmoothTransition);
+    //  Debug.Log("Loaded ecosystem for " + ecosystemName);
+
+    //  for (int scriptIndex = 0; scriptIndex < 100; scriptIndex++) {
+    //    string scriptPath = Path.Combine(textFolder.Path, ecosystemName + " " + scriptIndex + ".txt");
+    //    if (!File.Exists(scriptPath)) {
+    //      scriptPath = Path.Combine(textFolder.Path, ecosystemName + " 0" + scriptIndex + ".txt");
+    //    }
+
+    //    if (!File.Exists(scriptPath)) {
+    //      continue;
+    //    }
+
+    //    Debug.Log("Loaded text for " + Path.GetFileName(scriptPath));
+    //    textLabel.text = File.ReadAllText(scriptPath);
+    //  }
+    //}
+  }
+
+  void Update() {
+    if (Input.GetKeyDown(KeyCode.RightArrow)) {
+      OnNext();
     }
-    while (isButtonPressed) {
-      yield return null;
+
+    if (Input.GetKeyDown(KeyCode.LeftArrow)) {
+      OnPrev();
     }
 
-    var ecosystemPaths = Directory.GetFiles(ecosystemFolder.Path).
-                                   Where(p => p.EndsWith(".json")).
-                                   OrderBy(p => p).
-                                   ToList();
-
-    foreach (var ecosystemPath in ecosystemPaths) {
-      string ecosystemName = Path.GetFileNameWithoutExtension(ecosystemPath);
-
-      var desc = JsonUtility.FromJson<TextureSimulator.SimulationDescription>(File.ReadAllText(ecosystemPath));
-      sim.RestartSimulation(desc, TextureSimulator.ResetBehavior.SmoothTransition);
-      Debug.Log("Loaded ecosystem for " + ecosystemName);
-
-      for (int scriptIndex = 0; scriptIndex < 100; scriptIndex++) {
-        string scriptPath = Path.Combine(textFolder.Path, ecosystemName + " " + scriptIndex + ".txt");
-        if (!File.Exists(scriptPath)) {
-          scriptPath = Path.Combine(textFolder.Path, ecosystemName + " 0" + scriptIndex + ".txt");
-        }
-
-        if (!File.Exists(scriptPath)) {
-          continue;
-        }
-
-        Debug.Log("Loaded text for " + Path.GetFileName(scriptPath));
-        textLabel.text = File.ReadAllText(scriptPath);
-
-        while (!isButtonPressed) {
-          yield return null;
-        }
-        while (isButtonPressed) {
-          yield return null;
-        }
-      }
+    if (Input.GetKeyDown(KeyCode.DownArrow)) {
+      OnRestart();
     }
   }
 
-  private bool isButtonPressed {
-    get {
-      return nextButton.isPressed || Input.GetKey(KeyCode.F10);
+  public void OnNext() {
+    if (_currScript == _scriptPaths.Count - 1 && _currEcosystem == _ecosystemPaths.Count - 1) {
+      return;
     }
+
+    _currScript++;
+    if (_currScript >= _scriptPaths.Count) {
+      _currEcosystem++;
+      _currScript = 0;
+
+      loadScripts();
+      transition(TextureSimulator.ResetBehavior.ResetPositions);
+    }
+
+    updateText();
   }
+
+  public void OnPrev() {
+    if (_currEcosystem <= 0 && _currScript <= 0) {
+      return;
+    }
+
+    _currScript--;
+    if (_currScript < 0) {
+      _currEcosystem--;
+      loadScripts();
+      _currScript = _scriptPaths.Count - 1;
+
+      transition(TextureSimulator.ResetBehavior.ResetPositions);
+    }
+
+    updateText();
+  }
+
+  public void OnRestart() {
+    _currScript = 0;
+    transition(TextureSimulator.ResetBehavior.ResetPositions);
+    updateText();
+  }
+
+  private void loadScripts() {
+    string currEcosystemPath = _ecosystemPaths[_currEcosystem];
+    string ecosystemName = Path.GetFileNameWithoutExtension(currEcosystemPath);
+    _scriptPaths = Directory.GetFiles(textFolder.Path).
+                             Where(p => p.Contains(ecosystemName)).
+                             Where(p => p.EndsWith(".txt")).
+                             OrderBy(p => p.Replace(ecosystemName, "")).
+                             ToList();
+  }
+
+  private void transition(TextureSimulator.ResetBehavior resetBehavior) {
+    var desc = JsonUtility.FromJson<TextureSimulator.SimulationDescription>(File.ReadAllText(_ecosystemPaths[_currEcosystem]));
+    sim.RestartSimulation(desc, resetBehavior);
+  }
+
+  private void updateText() {
+    textLabel.text = File.ReadAllText(_scriptPaths[_currScript]);
+  }
+
+
+
 }

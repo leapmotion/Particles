f69ce37712d0366aa3a624e3ca134fde1e2aaa66
Alex Marcolina <amarcolina@gmail.com>
2017-08-23 17:38:01 -0700

    Added button highlighting

---
+++ Assets/ParticleEngine/TextureSimulator/Scripts/ButtonHighlight.cs
@@ -0,0 +1,29 @@
+﻿using UnityEngine;
+using Leap.Unity.Interaction;
+using Leap.Unity.GraphicalRenderer;
+
+[RequireComponent(typeof(InteractionButton))]
+public class ButtonHighlight : MonoBehaviour {
+
+  public LeapGraphic graphic;
+  public Color baseColor;
+  public Color primaryHoverColor;
+  public Color pressColor;
+
+  private LeapRuntimeTintData _tint;
+  private InteractionButton _button;
+
+  void Start() {
+    _tint = graphic.GetFeatureData<LeapRuntimeTintData>();
+    _button = GetComponent<InteractionButton>();
+  }
+
+  void Update() {
+    if (_button.isPressed) {
+      _tint.color = pressColor;
+    } else if (_button.isPrimaryHovered) {
+      _tint.color = primaryHoverColor;
+    } else
+      _tint.color = baseColor;
+  }
+}

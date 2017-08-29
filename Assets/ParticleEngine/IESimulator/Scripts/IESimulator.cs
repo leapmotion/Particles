3f7e10b9b42987be3eb753f3379d0acbeedf17f6
Amarcolina <Amarcolina@gmail.com>
2017-08-28 17:29:01 -0700

    working here

--- Assets/ParticleEngine/IESimulator/Scripts/IESimulator.cs
+++ Assets/ParticleEngine/IESimulator/Scripts/IESimulator.cs
@@ -1,6 +1,9 @@
-﻿using System.Collections;
+﻿using System.IO;
 using System.Collections.Generic;
 using UnityEngine;
+using Leap.Unity;
+using Leap.Unity.Query;
+using Leap.Unity.Attributes;
 
 public class IESimulator : MonoBehaviour {
   public const float PARTICLE_RADIUS = 0.01f;
@@ -9,8 +12,51 @@ public class IESimulator : MonoBehaviour {
   [SerializeField]
   private GameObject _particlePrefab;
 
-  private List<IEParticle> _particles;
+  [SerializeField]
+  private StreamingFolder _loadingFolder;
+
+  private List<IEParticle> _particles = new List<IEParticle>();
   private TextureSimulator.SimulationDescription _desc;
+  private float _currScale = 1;
+
+  public void LoadDescription(TextureSimulator.SimulationDescription desc) {
+    _desc = desc;
+
+    foreach (var obj in _particles) {
+      DestroyImmediate(obj.gameObject);
+    }
+    _particles.Clear();
+
+    foreach (var obj in desc.toSpawn) {
+      GameObject particle = Instantiate(_particlePrefab);
+      particle.transform.SetParent(transform);
+      particle.transform.localPosition = obj.position;
+      particle.transform.localRotation = Quaternion.identity;
+      particle.GetComponent<Rigidbody>().velocity = obj.velocity;
+      particle.GetComponent<IEParticle>().species = obj.species;
+      particle.SetActive(true);
+
+      _particles.Add(particle.GetComponent<IEParticle>());
+    }
+
+    ScaleBy(_currScale);
+  }
+
+  private void Update() {
+    if (Input.GetKeyDown(KeyCode.F6)) {
+      var path = Directory.GetFiles(_loadingFolder.Path).Query().FirstOrDefault(t => t.EndsWith(".json"));
+      var desc = JsonUtility.FromJson<TextureSimulator.SimulationDescription>(File.ReadAllText(path));
+      LoadDescription(desc);
+    }
+  }
+
+  private void ScaleBy(float ratio) {
+    foreach (var particle in _particles) {
+      particle.transform.position *= ratio;
+      particle.rigidbody.velocity *= ratio;
+      particle.transform.localScale *= ratio;
+    }
+  }
 
   private void FixedUpdate() {
     foreach (var particle in _particles) {
@@ -25,19 +71,19 @@ public class IESimulator : MonoBehaviour {
         float distance = toOther.magnitude;
         toOther = distance < 0.0001 ? Vector3.zero : toOther / distance;
 
-        if (distance < PARTICLE_DIAMETER) {
-          float penetration = 1 - distance / PARTICLE_DIAMETER;
+        if (distance < PARTICLE_DIAMETER * _currScale) {
+          float penetration = 1 - distance / (PARTICLE_DIAMETER * _currScale);
           float collisionScalar = (_desc.speciesData[particle.species].collisionForce + _desc.speciesData[other.species].collisionForce) * 0.5f;
-          collisionForce -= toOther * penetration * collisionScalar;
+          collisionForce -= toOther * penetration * collisionScalar * _currScale;
         }
 
-        if (distance < _desc.socialData[particle.species, other.species].socialRange) {
-          socialForce += toOther * _desc.socialData[particle.species, other.species].socialForce;
+        if (distance < _desc.socialData[particle.species, other.species].socialRange * _currScale) {
+          socialForce += toOther * _desc.socialData[particle.species, other.species].socialForce * _currScale;
           socialInteractions++;
         }
       }
 
-      particle.rigidbody.velocity += collisionForce;
+      particle.rigidbody.velocity += collisionForce / Time.fixedDeltaTime;
 
       if (socialInteractions > 0) {
         particle.forceBuffer.PushFront(socialForce / socialInteractions);
@@ -47,8 +93,10 @@ public class IESimulator : MonoBehaviour {
 
       if (particle.forceBuffer.Count > _desc.speciesData[particle.species].forceSteps) {
         particle.forceBuffer.PopBack(out socialForce);
-        particle.rigidbody.velocity += socialForce;
+        particle.rigidbody.velocity += socialForce / Time.fixedDeltaTime;
       }
+
+      particle.rigidbody.velocity *= (1.0f - _desc.speciesData[particle.species].drag);
     }
   }
 

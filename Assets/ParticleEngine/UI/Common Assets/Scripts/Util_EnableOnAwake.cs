176023e356b50509d91d2bf1cc466e0d0d263345
Nicholas Benson <nbenson@leapmotion.com>
2017-09-11 15:24:47 -0700

    Progress on UI rev 4

---
+++ Assets/ParticleEngine/UI/Common Assets/Scripts/Util_EnableOnAwake.cs
@@ -0,0 +1,15 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Util_EnableOnAwake : MonoBehaviour {
+
+  public GameObject[] objectsToEnable;
+
+  private void Awake() {
+    foreach (var obj in objectsToEnable) {
+      obj.SetActive(true);
+    }
+  }
+
+}

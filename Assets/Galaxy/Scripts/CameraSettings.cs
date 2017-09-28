ad8a72a6200754f8ab4bf0a72d22b22e7d295f07
Amarcolina <Amarcolina@gmail.com>
2017-09-28 10:11:22 -0700

    added camera settings

---
+++ Assets/Galaxy/Scripts/CameraSettings.cs
@@ -0,0 +1,20 @@
+﻿using UnityEngine;
+using Leap.Unity.DevGui;
+
+public class CameraSettings : MonoBehaviour {
+
+  [DevCategory("General Settings")]
+  [Range(0.001f, 0.4f)]
+  [DevValue]
+  public float nearClipPlane = 0.2f;
+
+  private Camera _camera;
+
+  private void Start() {
+    _camera = GetComponent<Camera>();
+  }
+
+  private void Update() {
+    _camera.nearClipPlane = nearClipPlane;
+  }
+}

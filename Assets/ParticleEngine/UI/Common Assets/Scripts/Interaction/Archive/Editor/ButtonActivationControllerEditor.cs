cece26e3b09294765db33a13561d464b5ccd14e0
Nicholas Benson <nbenson@leapmotion.com>
2017-11-06 14:48:59 -0800

    Migrate app-agnostic scripts to AppModules

---
+++ Assets/ParticleEngine/UI/Common Assets/Scripts/Interaction/Archive/Editor/ButtonActivationControllerEditor.cs
@@ -0,0 +1,18 @@
+﻿using Leap.Unity;
+using System.Collections;
+using System.Collections.Generic;
+using UnityEditor;
+using UnityEngine;
+
+[CustomEditor(typeof(ButtonActivationController))]
+public class ButtonActivationControllerEditor : CustomEditorBase<ButtonActivationController> {
+
+  protected override void OnEnable() {
+    base.OnEnable();
+  }
+
+  public override void OnInspectorGUI() {
+    base.OnInspectorGUI();
+  }
+
+}

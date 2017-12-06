18fafcd6dc0944af21c5e30136c6ed828097fa90
Alex Marcolina <amarcolina@gmail.com>
2017-12-06 15:57:59 -0800

    working on the palm tap gesture

--- Assets/Galaxy/Scripts/Gesture/PalmTapGesture.cs
+++ Assets/Galaxy/Scripts/Gesture/PalmTapGesture.cs
@@ -1,13 +1,13 @@
 ﻿using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using UnityEngine.Events;
 using Leap;
 using Leap.Unity;
 using Leap.Unity.Gestures;
-using Leap.Unity.Attributes;
+using Leap.Unity.RuntimeGizmos;
 
-
-public class PalmTapGesture : TwoHandedGesture {
+public class PalmTapGesture : TwoHandedGesture, IRuntimeGizmoComponent {
 
   public TappedHand tappedHand = TappedHand.Left;
   public TargetSide targetSide = TargetSide.Palm;
@@ -22,24 +22,11 @@ public class PalmTapGesture : TwoHandedGesture {
   public float hysteresis = 0.01f;
 
   protected override bool ShouldGestureActivate(Hand leftHand, Hand rightHand) {
-    var handThatIsTapped = tappedHand == TappedHand.Left ? leftHand : rightHand;
-    var handThatTaps = tappedHand == TappedHand.Left ? rightHand : leftHand;
-
-    return isHandInsideCylinder(handThatTaps, handThatIsTapped, 2, 2) &&
-          !isHandInsideCylinder(handThatTaps, handThatIsTapped, areaRadius, areaHeight - hysteresis * 0.5f);
+    throw new System.NotImplementedException();
   }
 
   protected override bool ShouldGestureDeactivate(Hand leftHand, Hand rightHand, out DeactivationReason? deactivationReason) {
-    var handThatIsTapped = tappedHand == TappedHand.Left ? leftHand : rightHand;
-    var handThatTaps = tappedHand == TappedHand.Left ? rightHand : leftHand;
-
-    if (!isHandInsideCylinder(handThatTaps, handThatIsTapped, areaRadius, areaHeight + hysteresis * 0.5f)) {
-      deactivationReason = DeactivationReason.FinishedGesture;
-      return true;
-    } else {
-      deactivationReason = null;
-      return false;
-    }
+    throw new System.NotImplementedException();
   }
 
   private bool isHandInsideCylinder(Hand hand, Hand target, float radius, float height) {
@@ -49,22 +36,45 @@ public class PalmTapGesture : TwoHandedGesture {
     Vector3 tipPos = hand.GetIndex().Bone(Bone.BoneType.TYPE_DISTAL).NextJoint.ToVector3();
     Vector3 deltaTip = tipPos - palmPos;
 
-    var tipSide = Vector3.Dot(deltaTip, normalVector) > 0 ? TargetSide.Palm : TargetSide.Back;
-    if (tipSide != targetSide) {
-      return false;
-    }
-
     Vector3 projected = Vector3.Project(deltaTip, normalVector);
 
     float heightAxis = projected.magnitude;
     float radialAxis = (deltaTip - projected).magnitude;
 
-    return heightAxis < height && radialAxis < radius;
+    return heightAxis < (height / 2) && radialAxis < radius;
+  }
+
+  public void OnDrawRuntimeGizmos(RuntimeGizmoDrawer drawer) {
+    var leftHand = Hands.Left;
+    var rightHand = Hands.Right;
+
+    var handThatIsTapped = tappedHand == TappedHand.Left ? leftHand : rightHand;
+    var handThatTaps = tappedHand == TappedHand.Left ? rightHand : leftHand;
+
+    if (handThatIsTapped == null) {
+      return;
+    }
+
+    switch (_state) {
+      case State.Inactive:
+        drawer.color = handThatTaps == null ? Color.gray : Color.white;
+        break;
+      case State.Hover:
+        drawer.color = Color.yellow;
+        break;
+      case State.Active:
+        drawer.color = Color.green;
+        break;
+    }
+
+
+
+    drawer.DrawSphere(handThatIsTapped.PalmPosition.ToVector3(), 0.02f);
   }
 
   public enum TargetSide {
-    Palm,
-    Back
+    Palm = 1,
+    Back = 2
   }
 
   public enum TappedHand {

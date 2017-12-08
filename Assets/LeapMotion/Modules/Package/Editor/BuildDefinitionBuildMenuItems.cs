fea12f2b2623bf1de4a7ef6ba053d8c33d2b93e5
Alex Marcolina <amarcolina@gmail.com>
2017-12-08 11:34:15 -0800

    Created base scene and added build menu

--- Assets/LeapMotion/Modules/Package/Editor/BuildDefinitionBuildMenuItems.cs
+++ Assets/LeapMotion/Modules/Package/Editor/BuildDefinitionBuildMenuItems.cs
@@ -9,6 +9,12 @@ namespace Leap.Unity.Packaging {
     public static void Build_d1d88f9c232529d4a87841795c181229() {
       BuildDefinition.Build("d1d88f9c232529d4a87841795c181229");
     }
+
+    // Solar System
+    [MenuItem("Build/Solar System", priority = 20)]
+    public static void Build_7eb6de42354fe7a40bd29dc50a3e3b0f() {
+      BuildDefinition.Build("7eb6de42354fe7a40bd29dc50a3e3b0f");
+    }
   }
 }
 

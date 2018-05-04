82d3b7aa58c1f89383a29e21ef6399edaead28e0
Nicholas Benson <nbenson@leapmotion.com>
2018-05-04 13:26:04 -0700

    Esc to Quit, run in BG, remove Unity splash screen

---
+++ Assets/ParticlesApp/ParticlesUI/Scripts/UI Controllers/PressEscToQuit.cs
@@ -0,0 +1,15 @@
+﻿using UnityEngine;
+
+namespace Leap.Unity.Particles {
+
+  public class PressEscToQuit : MonoBehaviour {
+
+    private void Update() {
+      if (Input.GetKeyDown(KeyCode.Escape)) {
+        Application.Quit();
+      }
+    }
+
+  }
+
+}
